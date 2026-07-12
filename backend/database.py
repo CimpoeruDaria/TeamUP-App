819fafbc14ea4f62789ee6be815787f5f58c14b0
CimpoeruDaria <dariamcimpoeru@gmail.com>
2026-07-12 13:08:50 +0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,14 @@
 import pymysql
 
-# Funcția care face legatura cu database
+# Funcția care face legatura cu database(cu serverul MySQL)
 def get_db_connection():
     connection = pymysql.connect(
         host="localhost",
         user="root",
         password="",  
-        database="teamup_db",  # baza de date din myPHPAdmin
+        database="teamup_db",  # database-ul din myPHPAdmin
         port=3306,
         cursorclass=pymysql.cursors.DictCursor # Returneaza datele ca dictionare
+                                    # adica salveaza informatiile sub forma Cheie:Valoare
     )
     return connection
